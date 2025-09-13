6511fbee61f39e9a39849c53a21a636f7e224e80
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-13 11:08:45 +0200

    Added DebugTool folder with a debug function example

---
+++ Addons/DebugTools/debug_world.gd
@@ -0,0 +1,15 @@
+@tool
+# This script and the Scene attached are use for debug examples.
+extends Node2D
+
+# Imagine the game need to reference the player from the start somehow.
+# We use @export to have a relative reference that doesn't risk to break.
+@export var reference_player: CharacterBody2D = null
+
+# This function use a condition to trigger a Warning int the editor's Scene tree.
+# It displays the "String" written under the if: return[].
+# You may need to reload the project for the editor to update and show the Warning.
+func _get_configuration_warnings() -> PackedStringArray:
+	if reference_player == null:
+		return ["The reference to the player is empty."]
+	return []
