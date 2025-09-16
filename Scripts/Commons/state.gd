d6cbf1a36d58d63f8370ea08e6a4b0bab6f916bd
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-16 22:10:43 +0200

    Created state.gd

---
+++ Scripts/Commons/state.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name State
+
+
+func _on_enter() -> void:
+	pass;
+
+func _on_process(_delta: float) -> void:
+	pass;
+
+func _on_physics_process(_delta: float) -> void:
+	pass;
+
+func _on_exit() -> void:
+	pass;
