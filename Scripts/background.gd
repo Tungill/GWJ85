cd74568202f804a0add54c88faf99ada700b399e
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-17 15:39:09 +0200

    Created Background

---
+++ Scripts/background.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+signal background_changed
+
+const SCALE_MATCHING_TRANSITION: Vector2 = Vector2(0.6, 0.6)
+
+@export var backgrounds: Array[Sprite2D]
+
+var current_background: int = 0
+
+
+func _ready() -> void:
+	# Reset backgrounds values.
+	for sprite: Sprite2D in backgrounds:
+		sprite.modulate.a = 1
+		sprite.visible = true
+
+#region DEBUG
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_action_1") and OS.is_debug_build():
+		change_background_transition(backgrounds[current_background+1])
+#endregion
+
+
+func change_background_transition(new_background: Sprite2D) -> void:
+	var sprite: Sprite2D = backgrounds[current_background]
+	var tween: Tween = create_tween()
+	tween.tween_property(sprite, "scale", SCALE_MATCHING_TRANSITION, 0.5).from_current()
+	tween.tween_property(sprite, "modulate", Color(1, 0), 0.5)
+	await tween.finished
+	backgrounds[current_background].visible = false
+	current_background = backgrounds.find(new_background) # Not sure it will works. Because all 3 are SPrite2D.
+	background_changed.emit()
