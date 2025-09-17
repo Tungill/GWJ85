0f3804a09acba0de044c90c01c3a281c409d3275
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-17 14:26:52 +0200

    Update Main Menu

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,16 +1,13 @@
 extends Control
 
-# TODO Copy Path of the game's Scene and past it between the "".
-const GAME_SCENE_PATH: StringName = "" # DEPRECATED
-
 @export var play_button: Button
-@export var settings_button: Button
-@export var settings_popup: Settings
+@export var pause_button: TextureButton
+@export var pause_popup: PauseMenu 
 
 
 func _ready() -> void:
 	play_button.pressed.connect(_on_play_button_pressed)
-	settings_button.pressed.connect(_on_settings_button_pressed)
+	pause_button.pressed.connect(_on_pause_button_pressed)
 
 # TBD 
 # Choice 1: Split MainMenu and Settings to be able to display them separately on the Game scene.
@@ -19,9 +16,8 @@ func _ready() -> void:
 # -> if we use another illustration for MainMenu's background.
 func _on_play_button_pressed() -> void:
 	self.visible = false
-	#get_tree().change_scene_to_file(GAME_SCENE_PATH) # DEPRECATED
 
 
-func _on_settings_button_pressed() -> void:
+func _on_pause_button_pressed() -> void:
 	get_tree().paused = true
-	settings_popup.toogle_visibility()
+	pause_popup.toogle_visibility()
