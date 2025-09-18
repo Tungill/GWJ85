6836ee6a5cfd87eda978a605c977377f03646e0e
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-18 12:12:01 +0200

    Updated Credits
    ~ Assets
    ~ Open/Close conditions
    + Open/Close animation

--- Scripts/UI/credits.gd
+++ Scripts/UI/credits.gd
@@ -1,13 +1,33 @@
 extends Control
 class_name Credits
 
-@export var close_button: Button
+const DEFAULT_POSITION_X: float = 0.0
 
+@export_category("Animations")
+@export var background: NinePatchRect
+@export var container: MarginContainer
+
+var background_size: Vector2
 
 func _ready() -> void:
 	self.visible = false
-	close_button.pressed.connect(toogle_visibility)
+	#region ANIMATIONS
+	background_size = background.size
+	var pre_animation_position_x: float = -background_size.x
+	background.position.x = pre_animation_position_x
+	container.position.x = pre_animation_position_x
+	#endregion
 
+func open_pop_up() -> void:
+	self.visible = true
+	var tween: Tween = create_tween()
+	tween.tween_property(background, "position", Vector2(0.0, 0.0), 0.3).from_current().set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(container, "position", Vector2(0.0, container.position.y), 0.3).from_current().set_ease(Tween.EASE_OUT)
 
-func toogle_visibility() -> void:
-	self.visible = !self.visible
+func close_pop_up() -> void:
+	var pre_animation_position_x: float = -background_size.x
+	var tween: Tween = create_tween()
+	tween.tween_property(background, "position", Vector2(pre_animation_position_x, 0.0), 0.5).from_current().set_ease(Tween.EASE_IN)
+	tween.parallel().tween_property(container, "position", Vector2(pre_animation_position_x, container.position.y), 0.5).from_current().set_ease(Tween.EASE_IN)
+	await tween.finished
+	self.visible = false
