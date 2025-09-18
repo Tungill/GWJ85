d34fa7598c76a493bd360334062ab3e4654d60c8
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-18 14:32:49 +0200

    Update credits.gd

--- Scripts/UI/credits.gd
+++ Scripts/UI/credits.gd
@@ -21,13 +21,13 @@ func _ready() -> void:
 func open_pop_up() -> void:
 	self.visible = true
 	var tween: Tween = create_tween()
-	tween.tween_property(background, "position", Vector2(0.0, 0.0), 0.3).from_current().set_ease(Tween.EASE_OUT)
+	tween.tween_property(background, "position", Vector2(0.0, background.position.y), 0.3).from_current().set_ease(Tween.EASE_OUT)
 	tween.parallel().tween_property(container, "position", Vector2(0.0, container.position.y), 0.3).from_current().set_ease(Tween.EASE_OUT)
 
 func close_pop_up() -> void:
 	var pre_animation_position_x: float = -background_size.x
 	var tween: Tween = create_tween()
-	tween.tween_property(background, "position", Vector2(pre_animation_position_x, 0.0), 0.5).from_current().set_ease(Tween.EASE_IN)
+	tween.tween_property(background, "position", Vector2(pre_animation_position_x, background.position.y), 0.5).from_current().set_ease(Tween.EASE_IN)
 	tween.parallel().tween_property(container, "position", Vector2(pre_animation_position_x, container.position.y), 0.5).from_current().set_ease(Tween.EASE_IN)
 	await tween.finished
 	self.visible = false
