62f71535b1ad412a0442b33f6e27eb39eb4e1fc4
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-20 15:42:07 +0200

    Update move.gd

--- Scripts/Enemies/Commons/move.gd
+++ Scripts/Enemies/Commons/move.gd
@@ -13,7 +13,7 @@ var directions: Dictionary[Direction, Vector2] = {
 	Direction.LEFT: Vector2.LEFT,
 	Direction.RIGHT: Vector2.RIGHT
 	}
-var player : CharacterBody2D
+var player: CharacterBody2D
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
@@ -22,14 +22,13 @@ func _on_enter() -> void:
 	pass
 	
 func _on_physics_process(delta: float) -> void:
-	# TODO Move on a straigth line until being able to reach the player when attacking
 	# NOTE If the player get out of range, the MoveState should be called again
-	if player == null: 
-		return
-	if player.global_position.x < mob.global_position.x:
-		move_direction = Direction.LEFT
-	else:
-		move_direction = Direction.RIGHT
+	if player:
+		if player.global_position.x < mob.global_position.x:
+			move_direction = Direction.LEFT
+		else:
+			move_direction = Direction.RIGHT
+	
 	var direction: Vector2 = directions[move_direction]
 	var motion: Vector2 = direction * speed * delta
 	motion.y = 0
