07111b7188ad62a9deef121c18ce5264e4e2a4fa
Tungill <60147117+Tungill@users.noreply.github.com>
2025-09-21 17:16:54 +0200

    Updated HUD
    + Made new HP display
    + Linked HP display to update with teh EventBus new signal

--- Scripts/UI/hud.gd
+++ Scripts/UI/hud.gd
@@ -1,13 +1,34 @@
 extends Control
 
+const HEART_FULL: CompressedTexture2D = preload("res://Assets/UI/T_Heart_Full.png")
+const HEART_EMPTY: CompressedTexture2D = preload("res://Assets/UI/T_Heart_Empty.png")
+
 @export var pause_button: TextureButton
 @export var pause_popup: PauseMenu 
+@export var health_point: Control
+@export var health_rect: TextureRect
+@export var health_label: Label
 
 
 func _ready() -> void:
 	pause_button.pressed.connect(_on_pause_button_pressed)
+	EventBus.player.health_changed.connect(_on_health_changed)
 
 
 func _on_pause_button_pressed() -> void:
 	get_tree().paused = true
 	pause_popup.toogle_visibility()
+
+
+func _on_health_changed(_old_value: int, new_value: int) -> void:
+	_update_health_points(new_value)
+
+
+func _update_health_points(new_value: int) -> void:
+	health_rect.texture = HEART_EMPTY
+	var tween: Tween = create_tween()
+	tween.tween_property(health_point, "scale", Vector2(1.2, 1.2), 0.25)
+	tween.tween_property(health_point, "scale", Vector2(1.0, 1.0), 0.25)
+	health_label.text = str(new_value)
+	await  tween.finished
+	health_rect.texture = HEART_FULL
