eca22fefadd1f62cbfcbb3b0146a8d358d339318
Zhi-704 <zhi.liang.704@gmail.com>
2025-09-21 15:26:35 +0100

    Added transition to victory screen

--- Scripts/UI/victory_screen.gd
+++ Scripts/UI/victory_screen.gd
@@ -2,18 +2,35 @@ extends Control
 
 @export var start_over_button: TextureButton
 @export var music_audio_player: AudioStreamPlayer
+@export var color_rect: ColorRect
+
 const GAME: PackedScene = preload("res://Levels/Game.tscn")
+const FADE_IN_DURATION: float = 3.0
+const FADE_OUT_DURATION: float = 5.0
 
 func _ready() -> void:
 	self.visible = false
 	EventBus.player.player_win.connect(_on_victory)
 	start_over_button.pressed.connect(_on_start_over_pressed)
+	color_rect.visible = false
+
+func _on_transition_begin() -> void:
+	color_rect.visible = true
+	var tween: Tween = create_tween()
+	tween.tween_property(color_rect, "modulate", Color(color_rect.color, 1.0), FADE_IN_DURATION).set_ease(Tween.EASE_OUT)
+	await  tween.finished
+
+func _on_transition_end() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(color_rect, "modulate", Color(color_rect.color, 0.0), FADE_OUT_DURATION).set_ease(Tween.EASE_IN)
+	await  tween.finished
+	color_rect.visible = false
 
 func _on_victory() -> void:
-	self.visible = true
+	_on_transition_begin()
 	get_tree().paused = true
+	self.visible = true
+	_on_transition_end()
 
 func _on_start_over_pressed() -> void:
-	# FIXME Triggers an error. SHould be the correct way to do it.
-	#get_tree().change_scene_to_packed(GAME) 
 	get_tree().reload_current_scene()
